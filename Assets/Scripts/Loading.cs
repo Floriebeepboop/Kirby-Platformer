f3bcf65f34428f100f147b7469e1183d56017e14
florie <f.poulet-souchard@etudiant.ecolemelies.fr>
2021-05-10 11:58:01 +0200

    create script 4 menu/loading/quit

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    public void ClickonPlay()
+    {
+        SceneManager.LoadScene("LevelKirby");
+    }
+
+    void Update()
+    {
+
+    }
+}
