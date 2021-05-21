ec9d829a8dc051a7329332eb807b56c65a4a4969
florie <f.poulet-souchard@etudiant.ecolemelies.fr>
2021-05-21 10:28:13 +0200

    trap script

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Trap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Reset()
     {
-        
+        GetComponent<BoxCollider2D>().isTrigger = true; //setting boxcollider trigger by default
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag=="Player")
+        {
+            Debug.Log($"{name}Triggered");
+        }
     }
+
 }
+
+//ttps://www.youtube.com/watch?v=3f-yrV7uoC4
