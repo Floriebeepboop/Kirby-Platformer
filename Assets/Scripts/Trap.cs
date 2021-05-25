e0c017965b53ab3a55bbf02cfbec8b3c578e4ad7
florie <f.poulet-souchard@etudiant.ecolemelies.fr>
2021-05-25 12:35:19 +0200

    rescale fire sprites

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Trap : MonoBehaviour
 {
+    private Animator Fire;
     private void Reset()
     {
         GetComponent<BoxCollider2D>().isTrigger = true; //setting boxcollider trigger by default
+        Fire = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag=="Player")
+        if(collision.tag == "Player")
         {
             Debug.Log($"{name}Triggered");
         }
     }
-
 }
 
 //ttps://www.youtube.com/watch?v=3f-yrV7uoC4
